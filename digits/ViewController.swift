09afc28ff0abd46ba881a7cff2500e7a20f93774
Serhii Kravchenko <serhiikravchenko@serhii-kravchenko.local>
2019-02-02 19:05:15 +0200

    L3 Block 1  Task 0

--- digits/ViewController.swift
+++ digits/ViewController.swift
@@ -10,17 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    func digits() {
-        
+    func whoIsBigger(firstDigit: Int, secondDigit: Int) {
+        print(firstDigit > secondDigit ? "\(firstDigit) is bigger than \(secondDigit)" : "\(secondDigit) is bigger than \(firstDigit)")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        whoIsBigger(firstDigit: 5, secondDigit: 10)
     }
-
     
-    
-
 }
 
