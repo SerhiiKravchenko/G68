314224cdc44b8f43de53f720f68ccd5e54ab4c4c
Serhii Kravchenko <serhiikravchenko@serhii-kravchenko.local>
2019-02-04 22:15:08 +0200

    L4 Assignment #3 Task 2

--- digits/ViewController.swift
+++ digits/ViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Count name
     func symbolCount(str: String) {
         print("Count symbols in your name = \(str.count)")
+    }
+    
+    //Patrynomic sex
+    func endOfPatronymic(str: String) {
+        if str.suffix(2) == "ич" {
+            print("You are male!")
+        }
+        else if str.suffix(2) == "на" {
+            print("You are female!")
+        }
+        else {
+            print("You are alien!")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         symbolCount(str: "Serhii")
+        endOfPatronymic(str: "Сергеевич")
     }
 
 }
