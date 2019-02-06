69a9060e71c13e5180b47455a7f895aa5481938f
Serhii Kravchenko <serhiikravchenko@serhii-kravchenko.local>
2019-02-06 22:34:24 +0200

    L4 Assignment #3 Task 6

--- digits/ViewController.swift
+++ digits/ViewController.swift
@@ -76,6 +76,39 @@ class ViewController: UIViewController {
         print(str)
     }
     
+    //password check
+    func passCheck(str: String) {
+        var symbols = ["numbers" : false, "lowSymb" : false, "capSym": false, "specSym" : false]
+        var count = 0
+
+        for scalar in str.unicodeScalars {
+            if 48...57 ~= scalar.value && symbols["numbers"] == false {
+                symbols["numbers"] = true
+                count += 1
+                print("Your pass has numbers")
+            }
+            if 97...122 ~= scalar.value && symbols["lowSymb"] == false {
+                symbols["lowSymb"] = true
+                count += 1
+                print("Your pass has lowercase symbols")
+            }
+            if 65...90 ~= scalar.value && symbols["capSym"] == false {
+                symbols["capSym"] = true
+                count += 1
+                print("Your pass has uppercase symbols")
+                
+            }
+            if (32...47 ~= scalar.value || 58...64 ~= scalar.value || 91...96 ~= scalar.value || 123...126 ~= scalar.value) && symbols["specSym"] == false {
+                symbols["specSym"] = true
+                count += 1
+                print("Your pass has specsymbols")
+            }
+        }
+        if count == 4 {
+            print("You have amazing password")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         symbolCount(str: "Serhii")
@@ -83,6 +116,7 @@ class ViewController: UIViewController {
         splitNameSurname(str: "SerhiiKravchenko")
         reverseString(str: "ReverseMe")
         numberSplitByComa(number: "12345678444444444444444444569")
+        passCheck(str: "ABn0,")
     }
 
 }
