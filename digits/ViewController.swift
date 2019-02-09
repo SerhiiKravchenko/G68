2d4aa03dca82b1d33c0db6a9db281b5af258a2e5
Serhii Kravchenko <serhiikravchenko@serhii-kravchenko.local>
2019-02-09 11:07:24 +0200

    L4 Addition to Assignment #2 Block 1 Task 2

--- digits/ViewController.swift
+++ digits/ViewController.swift
@@ -29,11 +29,31 @@ class ViewController: UIViewController {
         }
     }
     
+    //return summary count devides of number
+    func resultDivide(digit: Int, divide: Int) -> (Bool) {
+        return (digit % divide == 0 ? true : false)
+    }
+    
+    func countOfDivides(number: Int) -> Int{
+        var countDevides = 0
+        for i in 1...number {
+            if resultDivide(digit: number, divide: i) {
+                print("\(number) divided on \(i)")
+                countDevides += 1
+            }
+            else {
+                continue
+            }
+        }
+        return countDevides
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Biger number is = \(whoIsBigger(firstDigit: 5, secondDigit: 10))")
         print("Sqrt and cube of number = \(sqrtAndCube(number: 125))")
         printDigits(digit: 5)
+        print("Summary count of devides = \(countOfDivides(number: 10))")
     }
 }
 
